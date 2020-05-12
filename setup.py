a4cddfc62704e2beed9d05292abbca38b6f122ab
Victor Dibia <victor.dibia@gmail.com>
2020-05-11 22:17:41 -0700

    update folder dir

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 setuptools.setup(
     name='cmlbootstrap',
-    # packages=['cmlbootstrap'],
+    packages=setuptools.find_packages(),
     version='0.0.1',
     license='MIT',
     description='Wrapper class to launch jobs, experiments, applications and models on Cloudera Machine Learning',
-    author='Cloudera',
+    # author='Cloudera',
     url='https://github.com/fastforwardlabs/cmlbootstrap',
     # download_url='https://github.com/fastforwardlabs/cmlbootstrap/archive/v0.0.1.tar.gz',
     keywords=['CDSW', 'Cloudera', 'Machine Learning'],
